62ae59948ee9b51cee7021c1f69d4aafcbb53a06
LouisCR04 <louiskariuki04@gmail.com>
2023-09-21 23:14:15 +0300

    opcodes added

---
+++ op1codes.c
@@ -0,0 +1,143 @@
+#include "monty.h"
+
+
+/**
+ *_pint - prints the value at the top of the stack
+ *@stack: Pointer to the stack
+ *@line_number: Current line number of file
+ *
+ * Return: Void
+ */
+void _pint(stack_t **stack, unsigned int line_number)
+{
+	if ((*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		return;
+	}
+
+	printf("%d\n", (*stack)->next->n);
+}
+
+/**
+ *_swap - swaps the top two elements of the stack
+ *@stack: Pointer to the stack
+ *@line_number: Current line number of file
+ *
+ * Return: Void
+ */
+void _swap(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+
+	if ((*stack)->next == NULL || (*stack)->next->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't %s, stack too short\n", line_number, "swap");
+		return;
+	}
+
+	temp = (*stack)->next->next;
+	(*stack)->next->next = tmp->next;
+	(*stack)->next->prev = tmp;
+
+	if (temp->next)
+		temp->next->prev = (*stack)->next;
+
+	tmp->next = (*stack)->next;
+	tmp->prev = stack;
+	(*stack)->next = tmp;
+}
+
+/**
+ *_pop - removes the top element of the stack
+ *@stack: Pointer to the stack
+ *@line_number: Current line number of file
+ *
+ * Return: Void
+ */
+void _pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *next = NULL;
+
+	if ((*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
+		return;
+	}
+
+	next = (*stack)->next->next;
+	free((*stack)->next);
+
+	if (next)
+		next->prev = *stack;
+
+	(*stack)->next = next;
+}
+
+/**
+ *_pall -  prints all the values on the stack, from the top of the stack.
+ *@stack: Pointer to the stack
+ *@line_number: Current line number of file
+ *
+ * Return: Void
+ */
+void _pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp = (*stack)->next;
+
+	(void)line_number;
+	while (temp != NULL)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+	}
+}
+
+
+/**
+ *_push - Pushes an element to the stack
+ *@stack: Pointer to the stack
+ *@line_number: Current line number of file
+ *
+ * Return: Void
+ */
+void _push(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp, *new;
+	int i = 0;
+
+	new = malloc(sizeof(stack_t));
+	if (new == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		return;
+	}
+
+	if (tok[1] == NULL)
+	{
+		fprintf(stderr, "L%u: usage: push integer\n", line_number);
+		return;
+	}
+
+	while (tok[1][i] != NULL)
+	{
+		if (tok[1][i] == '-' && i == 0)
+			continue;
+		if (tok[1][i] < 'o' || tok[1][i] > '9')
+		{
+			fprintf(stderr, "L%u: usage: push integer\n", line_number);
+			return;
+		}
+
+		new->n = atoi(tok[1]);
+
+		temp = (*stack)->next;
+		new->prev = *stack;
+		new->next = temp;
+
+		if (temp != NULL)
+			temp->prev = new;
+
+		(*stack)->next = new;
+	}
+}
